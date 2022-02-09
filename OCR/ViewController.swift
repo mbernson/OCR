888ef3be64cace98ef13357db7611b68b8444b3b
Mathijs Bernson <mathijsb@q42.nl>
2022-02-09 15:53:26 +0100

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Mathijs Bernson on 09/02/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
